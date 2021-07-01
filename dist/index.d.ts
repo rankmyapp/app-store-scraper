0c436ff4ae4eccd329a9345af2264032358c1ccb
Bruno Ferreira <bruno.ferreira@rankmyapp.com.br>
2021-07-01 14:17:04 -0300

    update: rebuilt project :package:

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,5 +2,6 @@ export * from "./services/search.service";
 export * from "./services/list.service";
 export * from "./interfaces/app.interface";
 export * from "./interfaces/search.options.interface";
+export { Country } from "./interfaces/itunes.lookup.interface";
 export * from "./constants";
 //# sourceMappingURL=index.d.ts.map
