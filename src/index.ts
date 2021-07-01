cb587e467cebf3594f9bf71065c57c721c7d1b9b
Bruno Ferreira <bruno.ferreira@rankmyapp.com.br>
2021-07-01 14:16:55 -0300

    fix: exported missing type :bug:

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from "./services/list.service";
 // Interfaces
 export * from "./interfaces/app.interface";
 export * from "./interfaces/search.options.interface";
+export { Country } from "./interfaces/itunes.lookup.interface"
 
 // Constants
 export * from "./constants"
