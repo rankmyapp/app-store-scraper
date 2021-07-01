0e76c5d1742cd4eab6e05eb4f1311bf5c6fb11ed
Bruno Ferreira <bruno.ferreira@rankmyapp.com.br>
2021-07-01 12:31:59 -0300

    feat: added interfaces for category specific data :sparkles: :label:

---
+++ src/interfaces/category.options.interface.ts
@@ -0,0 +1,13 @@
+import { URLSearchParams } from "url";
+import { Category, Collection } from "../constants";
+import { Country } from "./itunes.lookup.interface";
+
+export interface CategoryOptions {
+  category: Category;
+  country?: Country;
+  collection?: Collection;
+  num?: number;
+  requestOptions?: URLSearchParams | Record<string, any>;
+  fullDetail?: boolean;
+  lang?: string;
+}
---
+++ src/interfaces/itunes.category.interface.ts
@@ -0,0 +1,129 @@
+interface Labeled {
+  label: string;
+}
+
+interface Image extends Labeled {
+  attributes: {
+    height: number;
+  };
+}
+
+interface Price extends Labeled {
+  attributes: {
+    amount: string;
+    currency: string;
+  };
+}
+
+interface ContentType {
+  attributes: {
+    term: string;
+    label: string;
+  };
+}
+
+interface EntryLinkAlternate {
+  attributes: {
+    rel: string;
+    type: string;
+    href: string;
+  };
+}
+
+interface EntryLink {
+  "im:duration": Labeled;
+  attributes: {
+    title: string;
+    rel: string;
+    type: string;
+    href: string;
+    "im:assetType": string;
+  };
+}
+
+interface LinkAlternate {
+  attributes: {
+    rel: string;
+    type: string;
+    href: string;
+  };
+}
+
+interface Link {
+  attributes: {
+    rel: string;
+    href: string;
+  };
+}
+
+interface ID extends Labeled {
+  attributes: {
+    "im:id": string;
+    "im:bundleId": string;
+  };
+}
+
+interface Artist extends Labeled {
+  attributes: {
+    href: string;
+  };
+}
+
+interface Category {
+  attributes: {
+    "im:id": string;
+    term: string;
+    scheme: string;
+    label: string;
+  };
+}
+
+interface ReleaseDate extends Labeled {
+  attributes: Labeled;
+}
+
+export interface ITunesCategoryResponseEntry {
+  "im:name": Labeled;
+  "im:image": Image[];
+  summary: Labeled;
+  "im:price": Price;
+  "im:contentType": ContentType;
+  rights: Labeled;
+  title: Labeled;
+  link: [EntryLinkAlternate, EntryLink];
+  id: ID;
+  "im:artist": Artist;
+  category: Category;
+  "im:releaseDate": ReleaseDate;
+}
+
+export interface ITunesCategoryResponse {
+  feed: {
+    author: { name: Labeled; uri: Labeled };
+    entry: ITunesCategoryResponseEntry[];
+    updated: Labeled;
+    rights: Labeled;
+    title: Labeled;
+    icon: Labeled;
+    link: [LinkAlternate, Link];
+    id: Labeled;
+  };
+}
+
+export interface ITunesCategoryInternalEntry {
+  id: string;
+  appId: string;
+  title: string;
+  icon: string;
+  url: any;
+  price: number;
+  currency: string;
+  free: boolean;
+  description?: string;
+  developer: string;
+  developerUrl?: string;
+  developerId?: string;
+  genre: string;
+  genreId: string;
+  released: string;
+}
